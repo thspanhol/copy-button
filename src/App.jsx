18df5e93a8f12a210cce131473f67f3ed4f738a8
Thales Spanhol <thales_spanhol@hotmail.com>
2023-07-21 15:11:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CopyButton from "./components/CopyButton";
+
+function App() {
+  return (
+    <>
+      <CopyButton text="Testando o texto copiado" />
+    </>
+  );
+}
+
+export default App;
