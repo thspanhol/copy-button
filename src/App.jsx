897bdcc12b1a22a3516af27183e4a2f723bf03ec
Thales Spanhol <thales_spanhol@hotmail.com>
2023-08-06 20:52:25 -0300

    Feat: array map

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import CopyButton from "./components/CopyButton";
 
 function App() {
+
+  const array = ['testeUm', 'testeDois', 'testeTres']
+
   return (
     <>
+      {array.map((e) => <CopyButton key={e} text={e} />)}
       <CopyButton text="Testando o texto copiado" />
     </>
   );
